9a7ba4850c9419f1226d43798292283f0f09a6c9
Lauren <laurenmontenegro10@gmail.com>
2018-05-13 03:44:16 -0600

    Created Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Menu from '../Menu';
+
+class Nav extends Component {
+    constructor(...props){
+        super(...props);
+    }
+
+    render() {
+        return (
+            <Menu/>
+        );
+    }
+}
