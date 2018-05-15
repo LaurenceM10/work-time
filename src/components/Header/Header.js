1ac37179ca30cd480bf3845e6fff8db968f786ec
Lauren <laurenmontenegro10@gmail.com>
2018-05-15 03:13:00 -0600

    Added Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+
+const Header = (props) => (
+    <div className="header">
+
+    </div>
+);
+
+export default Header;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './main.css';
+import Header from '../Header';
 import Nav from './../Nav';
 
 class Main extends Component {
@@ -11,6 +12,7 @@ class Main extends Component {
         return (
             <div className="main">
                 <Nav/>
+                <Header />
             </div>
         )
     }
